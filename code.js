08959146e77e9d66b5f5cf689a84bef4d0f126a7
Amr Ojjeh <amrojjeh@outlook.com>
2022-10-30 21:02:02 -0500

    Add logic

--- code.js
+++ code.js
@@ -1,37 +1,50 @@
+let player_score = 0;
+let computer_score = 0;
+
+let player_score_display = document.querySelector("#player_score");
+let computer_score_display = document.querySelector("#computer_score");
+let result_display = document.querySelector("#result");
+
 function getComputerChoice() {
 	let choice = Math.floor(Math.random() * 3);
 	return ["Rock", "Paper", "Scissors"][choice];
 }
 
+function toTitleCase(str) {
+	return `${str.toUpperCase()[0]}${str.toLowerCase().substr(1)}`;
+}
+
 function endRoundPrompt(hasWon, playerSelection, computerSelection) {
-	playerSelection = `${playerSelection.toUpperCase()[0]}${playerSelection.toLowerCase().substr(1)}`;
-	computerSelection = `${computerSelection.toUpperCase()[0]}${computerSelection.toLowerCase().substr(1)}`;
 	if (hasWon) {
-		return `You Win! ${playerSelection} beats ${computerSelection}`;
+		player_score += 1;
+		return `<span class="green">${playerSelection}</span> beats <span class="red">${computerSelection}</span>`;
 	} else {
-		return `You Lose! ${computerSelection} beats ${playerSelection}`;
+		computer_score += 1;
+		return `<span class="red">${computerSelection}</span> beats <span class="green">${playerSelection}</span>`;
 	}
 }
 
 function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection.toLowerCase();
-	computerSelection = computerSelection.toLowerCase();
+	playerSelection = toTitleCase(playerSelection);
+	computerSelection = toTitleCase(computerSelection);
 	if (computerSelection === playerSelection)
-		return `You Tie! Both of you played ${computerSelection}`;
+		return `You Tie! Both of you played <span class="yellow">${computerSelection}</span>`;
 	switch (playerSelection) {
-		case "rock":
-			return endRoundPrompt(computerSelection !== "paper", playerSelection, computerSelection);
-		case "paper":
-			return endRoundPrompt(computerSelection !== "scissors", playerSelection, computerSelection);
-		case "scissors":
-			return endRoundPrompt(computerSelection !== "rock", playerSelection, computerSelection);
+		case "Rock":
+			return endRoundPrompt(computerSelection !== "Paper", playerSelection, computerSelection);
+		case "Paper":
+			return endRoundPrompt(computerSelection !== "Scissors", playerSelection, computerSelection);
+		case "Scissors":
+			return endRoundPrompt(computerSelection !== "Rock", playerSelection, computerSelection);
 	}
 }
 
 function updateScore() {
-
+	player_score_display.innerText = `Your score: ${player_score}`;
+	computer_score_display.innerText = `Computer score: ${computer_score}`;
 }
 
 function play(playerSelection) {
-	console.log(playRound(playerSelection, getComputerChoice()));
+	result.innerHTML = playRound(playerSelection, getComputerChoice());
+	updateScore();
 }
